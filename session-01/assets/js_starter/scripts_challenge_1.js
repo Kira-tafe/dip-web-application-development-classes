4c22bbf35b5686635ec41ea49000fa131c22c251
Kira-tafe <j381269@tafe.wa.edu.au>
2025-10-02 15:01:27 +0800

    Update scripts_challenge_1.js

--- session-01/assets/js_starter/scripts_challenge_1.js
+++ session-01/assets/js_starter/scripts_challenge_1.js
@@ -3,16 +3,12 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Define the function doubleNumbers
-function doubleNumbers(arr) {
-  let doubled = arr.map((number) => number * 2);
-
-  return doubled;
- }
-
-let doubledNumbers = (arr) => arr.map((number) => number * 2);
-
+function doubleNumbers(arr){
+  const result = arr.map(num => num * 2) ;
+  return result
+}
 // Call the function with the numbers array
-let result = doubleNumbers(numbers);
-
+ let doubled = doubleNumbers(numbers);
 // Console log the new array
-console.log(result);
+console.log(doubled);
+console.log(numbers)
